dd2520e17748a330b9c2f70a282e4182e471a873
Jeremy Fowler <jembyfowler@gmail.com>
2024-07-15 09:50:54 -0600

    counter goes up

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
+<!-- NOTE each component's script file will act as a controller -->
 <script setup>
+import { ref } from 'vue';
 import { AppState } from './AppState'
 import Navbar from './components/Navbar.vue'
 
+// NOTE a ref creates a watchable object around whatever value is passed to to ref function
+const counter = ref(0)
+
+function increaseCounter() {
+  // .value accesses the value stored within the watchable object
+  counter.value++
+  console.log(counter);
+}
 
 </script>
 
@@ -10,7 +20,17 @@ import Navbar from './components/Navbar.vue'
     <Navbar />
   </header>
   <main>
-    <router-view />
+    <!-- <router-view /> -->
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <!-- NOTE when accessing a ref in the template tag, no .value is needed -->
+          <h1>Times Clicked: {{ counter }}</h1>
+          <!-- NOTE @click is the vue version of onclick -->
+          <button @click="increaseCounter()" class="btn btn-success">Click!</button>
+        </div>
+      </div>
+    </div>
   </main>
   <footer class="bg-dark text-light">
     Made with ☕ by Jeremy!
