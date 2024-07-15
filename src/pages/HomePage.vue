89b402a18eef44a8cb61cc2e222a22abf3881a37
Jeremy Fowler <jembyfowler@gmail.com>
2024-07-15 10:22:38 -0600

    stubbed out hero template

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,37 @@
+<!-- NOTE js goes here -->
 <script setup>
 
 </script>
 
+<!-- NOTE html goes here -->
 <template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-6 col-md-3">
+        <div class="bg-dark border border-light border-1 shadow-lg rounded text-light px-4">
+          <div class="text-center">
+            <img src="https://em-content.zobj.net/source/microsoft-teams/363/orangutan_1f9a7.png" alt=""
+              class="hero-img">
+          </div>
+          <p class="fs-4">Oslo</p>
+          <p class="d-flex justify-content-between fs-5r">
+            <span>HP:</span>
+            <span>0</span>
+          </p>
+          <p class="d-flex justify-content-between fs-5r">
+            <span>LVL:</span>
+            <span>0</span>
+          </p>
+        </div>
+      </div>
 
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<!-- NOTE css/scss goes here -->
+<style scoped lang="scss">
+.hero-img {
+  height: 20vh;
+}
+</style>
